d4e31eb93eac46fd693692377ec423e2fe4849b0
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-05-28 18:53:20 +0530

    extension working but all images changing

--- content.js
+++ content.js
@@ -3,18 +3,24 @@ const luffyImage= 'https://myblogiumbk1.s3.ap-southeast-2.amazonaws.com/images.j
 function replaceProfilePics() {
   const dynamiClassNames = [
     'EntityPhoto-circle-1',
+    'EntityPhoto-circle-0',
     'EntityPhoto-circle-2',
-    'EntityPhoto-circle-3'
+    'EntityPhoto-circle-3',
+    'EntityPhoto-square-3',
+    'EntityPhoto-square-0',
+    'EntityPhoto-square-1',
+    'EntityPhoto-square-2',
   ];
 
   dynamiClassNames.forEach(c => {
-    const classNameForThatProfileToChange = document.querySelectorAll(`ivm-view-attr__img--centered ${c}`);
-    classNameForThatProfileToChange.forEach(e => {
-      e.src = newImageUrl;
+    const classNameForThatProfileToChange = document.getElementsByClassName(`ivm-view-attr__img--centered`);
+    console.log(classNameForThatProfileToChange);  
+    Array.from(classNameForThatProfileToChange).forEach(e => {
+      e.src = luffyImage;
     });
   });
 }
-
+console.log("hilo")
 replaceProfilePics();
 
 const observer = new MutationObserver(replaceProfilePics);
